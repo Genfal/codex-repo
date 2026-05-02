3f1d13f43ab0dc52dddbff447b3f43b1b3c6e7a8
Genfal <phyoletovbiypingvin@gmail.com>
2026-05-02 15:52:49 +0300

    Init

---
+++ src/client/java/ru/andruha/client/DiamondClient.java
@@ -0,0 +1,10 @@
+package ru.andruha.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiamondClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
